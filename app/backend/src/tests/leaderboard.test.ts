3049fdd3fbd3614141bf233904b414be50127f4f
Gabriel Toscano <gabrieltoscano1@outlook.com>
2023-04-04 12:29:04 -0300

    [Feat] :rocket: Terminado a cobertura de testes do leaderboard

--- app/backend/src/tests/leaderboard.test.ts
+++ app/backend/src/tests/leaderboard.test.ts
@@ -10,7 +10,7 @@ chai.use(chaiHttp);
 const { expect } = chai;
 
 describe('GET /leaderboard', () => {
-  describe('Retornando ta classificação baseado nos times da casa', () => {
+  describe('Retornando ta classificação baseado na requisição', () => {
     it('Retornando status 200 quando é feita a requisição para /home', async () => {
       const httpResponse = await chai.request(app).get('/leaderboard/home');
       expect(httpResponse.status).to.equal(200);
@@ -18,6 +18,11 @@ describe('GET /leaderboard', () => {
 
     it('Retornando status 200 quando é feita a requisição para /away', async () => {
       const httpResponse = await chai.request(app).get('/leaderboard/away');
+      expect(httpResponse.status).to.equal(200);
+    });
+
+    it('Retornando status 200 quando é feita a requisição para /leaderboard', async () => {
+      const httpResponse = await chai.request(app).get('/leaderboard');
       expect(httpResponse.status).to.equal(200);
     });
   });
